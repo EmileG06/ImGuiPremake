dc44e6fef1537798b50bc4d2491d8bc38623b8fe
EmileG06 <emilegodin36@gmail.com>
2026-01-18 12:11:54 -0500

    Repo creation

---
+++ premake5.lua
@@ -0,0 +1,40 @@
+project "imgui"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++23"
+
+	systemversion "latest"
+	staticruntime "On"
+	toolset "v145"
+
+	flags { "MultiProcessorCompile" }
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	defines
+	{
+		"_CRT_SECURE_NO_WARNINGS"
+	}
+
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp"
+	}
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
